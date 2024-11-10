0804ad045572687ba3cbee2ca93eb39b5eeb3db9
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-10 16:59:53 +0100

    add iFrame

---
+++ components/nodes/iFrame.js
@@ -0,0 +1,60 @@
+import { mergeAttributes, Node } from "@tiptap/core";
+
+export const IFrame = Node.create({
+  name: "iframe",
+
+  group: "block",
+
+  atom: true,
+
+  addAttributes() {
+    return {
+      src: {
+        default: null,
+      },
+      frameBorder: {
+        default: "0",
+      },
+      allowFullscreen: {
+        default: "true",
+      },
+      width: {
+        default: "100%",
+      },
+      height: {
+        default: "400px",
+      },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: "iframe",
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return [
+      "div",
+      { class: "iframe-wrapper" },
+      ["iframe", mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)],
+    ];
+  },
+
+  addCommands() {
+    return {
+      setIframe:
+        (options) =>
+        ({ commands }) => {
+          return commands.insertContent({
+            type: this.name,
+            attrs: options,
+          });
+        },
+    };
+  },
+});
+
+export default IFrame;
