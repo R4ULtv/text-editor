7a9f1c21fa0afa0c4609a909f448c9706870308b
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-22 17:50:34 +0100

    add dialog provider

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import fs from "fs";
 
 import Editor from "@/components/Editor";
+import { DialogProvider } from "@/components/dialogs/provider";
 
 export default function Home() {
   const htmlContent = fs.readFileSync(`./app/blog-example.html`, "utf-8");
-  return <Editor html={htmlContent} />;
+  return (
+    <DialogProvider>
+      <Editor html={htmlContent} />
+    </DialogProvider>
+  );
 }
