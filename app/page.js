b5940213919396819934575aa463263996323f76
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-23 15:12:01 +0100

    add audio provider

--- app/page.js
+++ app/page.js
@@ -2,12 +2,15 @@ import fs from "fs";
 
 import Editor from "@/components/Editor";
 import { DialogProvider } from "@/components/dialogs/provider";
+import { AudioProvider } from "@/components/audio/provider";
 
 export default function Home() {
   const htmlContent = fs.readFileSync(`./app/blog-example.html`, "utf-8");
   return (
-    <DialogProvider>
-      <Editor html={htmlContent} />
-    </DialogProvider>
+    <AudioProvider>
+      <DialogProvider>
+        <Editor html={htmlContent} />
+      </DialogProvider>
+    </AudioProvider>
   );
 }
