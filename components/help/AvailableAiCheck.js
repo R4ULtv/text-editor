4a4603b21ee151346f0045ae08a9353c9b8d7486
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-24 16:57:53 +0100

    add ai check

---
+++ components/help/AvailableAiCheck.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
+export const AvailableAiCheck = () => {
+  const [availableAi, setAvailableAi] = useState(null);
+
+  const checkAiAvailability = async () => {
+    try {
+      if (window?.ai) {
+        const languageModel = await window.ai.languageModel.capabilities();
+        setAvailableAi(languageModel.available);
+        if (languageModel.available === "no") {
+          await window.ai.languageModel.create();
+        }
+      } else {
+        setAvailableAi("no");
+      }
+    } catch (error) {
+      setAvailableAi("no");
+    }
+  };
+  return (
+    <button
+      disabled={availableAi === "readily"}
+      onClick={checkAiAvailability}
+      className="px-1.5 py-0.5 bg-zinc-200 dark:bg-zinc-800 rounded-lg cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-700"
+    >
+      {availableAi === "readily"
+        ? "AI is ready to use"
+        : availableAi === "afterDownload"
+          ? "Still installing"
+          : "Check if AI is installed"}
+    </button>
+  );
+};
