906b9d04f919119e259236b4af4ac20940ab3070
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-25 10:00:09 +0100

    removed blog html

--- app/page.js
+++ app/page.js
@@ -1,15 +1,14 @@
-import fs from "fs";
-
 import Editor from "@/components/Editor";
 import { DialogProvider } from "@/components/dialogs/provider";
 import { AudioProvider } from "@/components/audio/provider";
 
+import { json } from "@/app/blog-starter";
+
 export default function Home() {
-  const htmlContent = fs.readFileSync(`./app/blog-example.html`, "utf-8");
   return (
     <AudioProvider>
       <DialogProvider>
-        <Editor html={htmlContent} />
+        <Editor content={json} />
       </DialogProvider>
     </AudioProvider>
   );
